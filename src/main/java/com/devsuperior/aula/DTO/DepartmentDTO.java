39b5cc6f616a11d966074bc5d6546ae095caa024
Carlos Fernandes <itais1@hotmail.com>
2025-11-29 14:38:18 +0000

    Savar para um

---
+++ src/main/java/com/devsuperior/aula/DTO/DepartmentDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.aula.DTO;
+
+import com.devsuperior.aula.entities.Department;
+
+public class DepartmentDTO {
+
+	
+	
+	private Long id;
+	
+	private String name;
+
+	public DepartmentDTO(Long id, String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+	
+public DepartmentDTO(Department entity) {
+		
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+	
+	
+}
---
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.aula.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.aula.DTO.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.PersonRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PersonService {
+	
+	
+	@Autowired
+	private PersonRepository repository;
+	
+	
+	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+		
+		Person entity = new Person();
+		
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+		
+		Department dept = new Department();
+		dept.setId(dto.getDepartment().getId());
+	
+		entity.setDepartment(dept);
+		
+		entity = repository.save(entity);
+		
+		return new PersonDepartmentDTO(entity);
+	}
+	
+	
+
+}
