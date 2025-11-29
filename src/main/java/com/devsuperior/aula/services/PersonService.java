1b129f74069ff9f24dd457fe526fc1d2d3bef342
Carlos Fernandes <itais1@hotmail.com>
2025-11-29 15:25:55 +0000

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -3,13 +3,12 @@ package com.devsuperior.aula.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.aula.DTO.PersonDTO;
 import com.devsuperior.aula.DTO.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
 import com.devsuperior.aula.repositories.DepartmentRepository;
 import com.devsuperior.aula.repositories.PersonRepository;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PersonService {
@@ -38,6 +37,27 @@ public class PersonService {
 		entity = repository.save(entity);
 		
 		return new PersonDepartmentDTO(entity);
+	}
+	
+	
+	
+public PersonDTO insert(PersonDTO dto) {
+		
+		Person entity = new Person();
+		
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+		
+		//Department dept =  deprepository.getReferenceById(dto.getDepartmentId());
+		
+		Department dept = new Department();
+		dept.setId(dto.getDepartmentId());
+	
+		entity.setDepartment(dept);
+		
+		entity = repository.save(entity);
+		
+		return new PersonDTO(entity);
 	}
 	
 	
